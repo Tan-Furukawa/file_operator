f46fa0fabf39af9b27cba7301fbd09b2e2acb443
Tan-Furukawa <furukawatan@gmail.com>
2024-08-14 17:19:03 +0900

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='file_operator',  # パッケージ名（pip listで表示される）
-    version="0.0.1",  # バージョン
+    version="0.0.2",  # バージョン
     description="",  # 説明
     author='Furukawa Tan',  # 作者名
     packages=find_packages(),  # 使うモジュール一覧を指定する
